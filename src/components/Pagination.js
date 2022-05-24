64eeec1e0831482ad17d300a43daa02858e8c108
Stefano Corra <stefano.corra@hotmail.com>
2022-05-23 21:46:20 -0400

    pagination component

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -5,18 +5,21 @@ const Pagination = (props) => {
   const globalState = props.globalState
   const posts = globalState.filterData
   const pagePosts = 4
+  const pages = posts.length / pagePosts
 
   let pagesArray = []
-  for (let number = 1; number <= posts.length / pagePosts; number++) {
-    pagesArray.push(
-      <Link to={`/details/${number}`} key={number}>
-        {number === 1 ? (
-          <li className='active'>{number}</li>
-        ) : (
-          <li>{number}</li>
-        )}
-      </Link>
-    )
+  if (posts.length >= 1) {
+    for (let number = 1; number <= pages; number++) {
+      pagesArray.push(
+        <Link to={`/details/${number}`} key={number}>
+          {number === 1 ? (
+            <li className='active'>{number}</li>
+          ) : (
+            <li>{number}</li>
+          )}
+        </Link>
+      )
+    }
   }
 
   return (
